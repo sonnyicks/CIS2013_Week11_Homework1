4774f246172b6ba63e692e5550da6398b2fdc52e
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-04-08 22:21:15 -0500

    void functions added

--- CIS2013_Week11_Homework1.cpp
+++ CIS2013_Week11_Homework1.cpp
@@ -10,31 +10,32 @@ class Car{
 	int year;
 	int mph;
 	bool running;
-	void user_choice(char);
-	void set_make(Car);
-	void set_model(Car);
-	void set_year(Car);
-	void start_car(bool);
-	void stop_car(bool);
-	void speed_change (Car);
-};
-
-class Operate{
-
+	void set_make();
+	void set_model();
+	void set_year();
+	void start_car();
+	void stop_car();
+	void speed_change();
 };
 
 Car user {"Blue", "Nissan", "Sentra", 2013, 0};
 
 char yesno = 'y';
+char user_choice;
+bool program_running = true;
 
 void print_car(Car);
 void instruct();
+void choose(char);
 
 int main(){
 	
 	print_car(user);
 	instruct();
-	print_car(user);
+	while (program_running){
+		cout << "What would you like to do?: ";
+		cin >> user_choice;
+	}
 	
 	return 0;
 }
@@ -47,5 +48,34 @@ void print_car(Car a){
 
 void instruct(){
 	cout << "Select an action: \n" << "	set make (m)\n	set model (o)\n	set year (y)\n"
-		 << "	start car (s)\n 	stop car (S)\n	print (p)\nWhat would you like to do?: ";
+		 << "	start car (s)\n 	stop car (S)\n	print (p)\n";
 }
+
+void choose(char a){
+	if (a=='m'){user.set_make();}
+}
+
+void Car::set_make(){cin >> user.make;}
+void Car::set_model(){cin >> user.model;}
+void Car::set_year(){cin >> user.year;}
+void Car::start_car(){user.running=true;}
+void Car::stop_car(){user.running=false;}
+void Car::speed_change(){
+	cout << "How fast do you want to go?: ";
+	cin >> user.mph;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
