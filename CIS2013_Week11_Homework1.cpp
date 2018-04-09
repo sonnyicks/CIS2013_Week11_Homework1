3e88994e1a3ea30193cc3cfd61f3507dc39824df
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-04-08 21:35:47 -0500

    instructions print

--- CIS2013_Week11_Homework1.cpp
+++ CIS2013_Week11_Homework1.cpp
@@ -15,6 +15,7 @@ Car user;
 char yesno = 'y';
 
 void print(Car);
+void instruct();
 
 int main(){
 	
@@ -31,6 +32,7 @@ int main(){
 		cout << "What color?";
 		cin >> user.color;
 	}
+	instruct();
 	print(user);
 	
 	return 0;
@@ -40,4 +42,9 @@ void print(Car a){
 	cout << "Color: " << a.color << endl << "Make: " << a.make << endl << "Model: "
 		 << a.model << endl << "Year: " << a.year << endl << "Current speed: " 
 		 << a.mph << " MPH" << endl;
+}
+
+void instruct(){
+	cout << "Select an action: \n" << "	set make (m)\n	set model (o)\n	set year (y)\n"
+		 << "	start car (s)\n 	stop car (S)\n	print (p)\nWhat would you like to do?: ";
 }
